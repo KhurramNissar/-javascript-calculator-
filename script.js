d8bb883072ba601d49e7304812bf82e091007a86
khurramgit initgit config --global user.name khurram <Khuramnissar@gmail.com>
2022-12-26 21:38:09 +0500

    [3rd]

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const actions = document.querySelector(".actions");
 const ans = document.querySelector(".ans");
 console.log(actions);
 console.log(ans);
+let history = document.getElementById("history");
+// creating function to append data in history box
+function calculation(v) {
+  var para = document.createElement("p");
+  var data = v + " = " + eval(v);
+  para.innerText = data;
+  history.appendChild(para);
+  return eval(v);
+}
 
 let expression = "";
 let a = 0;
 actions.addEventListener("click", (e) => {
   console.log(e.target);
   const value = e.target.dataset["value"];
+  // let historydata = [];
+  // historydata.push(value),
+  //   (document.getElementById("history-log").innerHTML = historydata);
 
   if (value !== undefined) {
     ///////
@@ -26,6 +38,8 @@ actions.addEventListener("click", (e) => {
     } else if (value == "=") {
       const answer = eval(expression).toFixed(4);
       expression = answer;
+    } else if (value == "His") {
+      calculation(expression);
     } else {
       expression += value;
     }
@@ -44,28 +58,30 @@ const square = () => {
   return eval(expression * expression).toFixed(4);
 };
 ////////////////history/////////////////////
-var historyData = [];
-var expressionData = "";
-var resultData = "";
+// var historyData = [];
+// var expressionData = "";
+// var resultData = "";
 /////////////////////////////////////////////
-expressionData = expression; // history
-resultData = answer; // history
-historyData.push({ expression: expressionData, result: resultData }); /// histrory
-showLogdata();
-resultData = ""; //history
-expressionData = ""; //history
-function showLogdata() {
-  var string = "";
-  var log = document.getElementById("history-log");
-  for (key in historyData) {
-    string +=
-      "" +
-      historyData[key]["expression"] +
-      "=" +
-      historyData[key]["result"] +
-      "<br>";
-  }
-  log.innerHTML = string;
-}
-console.log(historyData);
-document.getElementById("history").innerHTML = html;
+// expressionData = expression; // history
+// resultData = answer; // history
+// historyData.push[ expression ]; /// histrory
+// showLogdata();
+// resultData = ""; //history
+// expressionData = ""; //history
+// function showLogdata() {
+//   var string = "";
+//   var log = document.getElementById("history-log");
+//   for (key in historyData) {
+//     string +=
+//       "" +
+//       historyData[key]["expression"] +
+//       "=" +
+//       historyData[key]["result"] +
+//       "<br>";
+//   }
+//   log.innerHTML = string;
+// }
+// console.log(historyData);
+// document.getElementById("history").innerHTML = html;
+
+// document.addEventListener("His", function () {});
